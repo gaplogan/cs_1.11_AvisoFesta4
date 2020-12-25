d30aa908169c9ba92505254c994bcea1a57c1dbb
Gabriel <gaplogan@gmail.com>
2020-12-25 14:25:57 -0300

    2020.12.25.14.25

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace cs_1._11_AvisoFesta4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            for (int j = 0; j < 5; j++)
+            { //início do loop
+                //escreve a frase
+                Console.WriteLine("Hoje vai ter festa na casa da Fabi");
+            } //fim do loop           
+        }
+    }
+}
